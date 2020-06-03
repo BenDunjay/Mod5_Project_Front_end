a94c3e7d9204e201973ee21307efb71b61884d6e
BenDunjay <bendunjay@outlook.com>
2020-06-03 21:30:24 +0100

    Created Routes and Links displaying Login and SignUp Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
-
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/"> Home </Link>
+        <Link to="/signup"> Sign Up </Link>
+        <Link to="/login"> Log In </Link>
+
+        <Switch>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/login">
+            <App />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
