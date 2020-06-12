1bff4833b306ce35ad653154efd241a93e91f435
BenDunjay <bendunjay@outlook.com>
2020-06-12 12:48:51 +0100

    booking requests functional

--- src/components/ManageArtistBookings.js
+++ src/components/ManageArtistBookings.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
+import { Table } from "semantic-ui-react";
 
 import Request from "./Request";
 
 export default class ManageArtistBookings extends Component {
   displayBookings = () => {
     return this.props.artist.requests.map((request) => (
-      <li>
-        <Request request={request} />
-      </li>
+      <Request request={request} key={request.id} />
     ));
   };
 
   render() {
     return (
       <div>
-        Hi from Artist Bookings
-        <ul>{this.displayBookings()}</ul>
+        <Table striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Request Id</Table.HeaderCell>
+              <Table.HeaderCell>Date</Table.HeaderCell>
+              <Table.HeaderCell>Venue Name</Table.HeaderCell>
+              <Table.HeaderCell>Hours Needed</Table.HeaderCell>
+              <Table.HeaderCell>Payment Offer</Table.HeaderCell>
+              <Table.HeaderCell>Venue message</Table.HeaderCell>
+              <Table.HeaderCell>Accept?</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>{this.displayBookings()}</Table.Body>
+        </Table>
       </div>
     );
   }
