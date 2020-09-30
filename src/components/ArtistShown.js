a86d1916376d45b1edca370d83186097f759d57c
BenDunjay <bendunjay@outlook.com>
2020-09-30 19:29:13 +0100

    git card

--- src/components/ArtistShown.js
+++ src/components/ArtistShown.js
@@ -9,16 +9,27 @@ export default function ArtistShown(props) {
   return (
     <Card className="artist-shown-card">
       <Image src={props.artist.profile_picture} wrapped ui={false} />
-      <Card.Content>
+      <Card.Content className="artist-card-content">
         {/* <h3>This is {props.artist.name}'s profile Page </h3> */}
-        <Card.Header>{props.artist.username}</Card.Header>
-        <Card.Meta>
+        <Card.Header className="artist-card-content">
+          {props.artist.username}
+        </Card.Header>
+        <Card.Meta className="artist-card-content">
           <span>{props.artist.name}</span>
         </Card.Meta>
-        <Card.Meta> {props.artist.artist_genre}</Card.Meta>
-        <Card.Description> {props.artist.bio}</Card.Description>
-        <button>
-          <Link to={`/artist_profile/${props.artist.name}`}>
+        <Card.Meta className="artist-card-content">
+          {" "}
+          {props.artist.artist_genre}
+        </Card.Meta>
+        <Card.Description className="artist-card-content">
+          {" "}
+          {props.artist.bio}
+        </Card.Description>
+        <button className="artist-card-content-button">
+          <Link
+            to={`/artist_profile/${props.artist.name}`}
+            className="artist-card-content"
+          >
             Artist Profile
           </Link>
         </button>
